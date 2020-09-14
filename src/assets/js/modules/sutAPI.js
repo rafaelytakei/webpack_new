04c19278c8c58f8d252807821692ae509bef0298
Rafael Takei <rafael@suthub.com>
2020-09-14 11:30:13 -0300

    Documentação do module sutAPI

--- src/assets/js/modules/sutAPI.js
+++ src/assets/js/modules/sutAPI.js
@@ -1,8 +1,40 @@
-import Axios from 'axios';
+import axios from 'axios';
 import { apiURL, env } from './env';
 
-const sutAPI = Axios.create({
+const sutAPI = axios.create({
 	baseURL: `${apiURL}${env}`,
 });
 
 export default sutAPI;
+
+// Exemplo de chamada GET para API - sutAPI.get(endpoint, options)
+// sutAPI
+// 	.get(`/plan/`, {
+// 		headers: {
+// 			token: 'Colocar Token Aqui',
+// 		},
+// 	})
+// 	.then((res) => {
+// 		console.log(res.data);
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 	});
+
+// Exemplo de chamada POST para API - sutAPI.post(endpoint, payload, options)
+// const payload = {
+// 	nome: 'João',
+// };
+// sutAPI
+// 	.post(`/plan/`, JSON.stringify(payload), {
+// 		headers: {
+// 			token: 'Colocar Token Aqui',
+// 			'Content-Type': 'application/json',
+// 		},
+// 	})
+// 	.then((res) => {
+// 		console.log(res.data);
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 	});
